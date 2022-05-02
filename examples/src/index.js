c76e6ad232b501d679073db4fd6105f7fc0fbe05
SunLxy <1011771396@qq.com>
2022-05-03 07:14:07 +0800

    fix:注释

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,7 +3,7 @@ import "./index.less";
 import "./index.css";
 import ReactClient from "react-dom/client";
 import App from "./app/App";
-import Dmode from "./.docs/examplessrcappApp";
+// import Dmode from "./.docs/examplessrcappApp";
 // import Dmode2 from "./.docs/examplesREADME";
 // import Dmode2 from "./da4";
 ReactClient.createRoot(document.getElementById("root")).render(
